a157814a4ee2e46e55f113807105f32fc257cd6e
Nikos <niqos89@gmail.com>
2025-04-30 21:02:44 +0300

    feat: changes to output data to subcomponents

--- src/app/components/person-table/person-table.component.ts
+++ src/app/components/person-table/person-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { EPerson } from 'src/app/shared/interfaces/eperson';
 import { Person } from 'src/app/shared/interfaces/person';
 
 @Component({
@@ -8,13 +9,31 @@ import { Person } from 'src/app/shared/interfaces/person';
   styleUrl: './person-table.component.css'
 })
 export class PersonTableComponent {
-  @Input() personInput: Person | undefined
+  @Input() personInput: Person | EPerson | undefined
   name = "Margie"
 
   person = {
     givenName: "Margie",
     surname: "Dmtk",
     age: 33,
-    email: "margie@aueb.gr"
+    email: "margie@aueb.gr",
+    
+  }
+  addressOrEducation: string | undefined;
+
+  isPerson(): boolean {
+    if (this.personInput && 'address' in this.personInput) {
+      this.addressOrEducation = this.personInput.address;
+      return true;
+    }
+    return false;
+  }
+ 
+  isEPerson(): boolean {
+    if (this.personInput && 'education' in this.personInput) {
+      this.addressOrEducation = this.personInput.education;
+      return true;
+    }
+    return false;
   }
 }
