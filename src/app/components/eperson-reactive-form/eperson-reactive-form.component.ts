19c46716de856e854a561dc6dc57ac31dc082644
Nikos <niqos89@gmail.com>
2025-05-06 20:08:48 +0300

    feat: reactive form

--- src/app/components/eperson-reactive-form/eperson-reactive-form.component.ts
+++ src/app/components/eperson-reactive-form/eperson-reactive-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl,FormGroup,ReactiveFormsModule,Validators } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
@@ -15,7 +15,9 @@ import { EPerson } from 'src/app/shared/interfaces/eperson';
   styleUrl: './eperson-reactive-form.component.css'
 })
 export class EpersonReactiveFormComponent {
-  
+  @Output() person = new EventEmitter<EPerson>();
+
+  // 1ος τροπος (most common)
   // το form ειναι δικο μας ονομα θα μπορουσε να ειναι το οτιδηποτε
   form = new FormGroup({
     // στην παρενθεση βαζουμε αν θελουμε καποιο default value
@@ -32,10 +34,39 @@ export class EpersonReactiveFormComponent {
     education: new FormControl("", Validators.required)
   })
 
-  onSubmit(data: any) {
-    console.log("Data: ", data)
-    console.log("givenName>>>", this.form.controls["givenName"].value);
-    this.form.controls["surName"].setValue("Papakis");
+  // 2os Τρόπος
+  // form = new FormGroup<{
+  //   givenName: FormControl<string>,
+  //   surName: FormControl<string>,
+  //   email: FormControl<string>,
+  //   age: FormControl<number>,
+  //   education: FormControl<string>,
+  // }>({
+  //   givenName: new FormControl("", {nonNullable:true, validators: Validators.required}),
+  //   surName: new FormControl("", {nonNullable:true, validators: Validators.required}),
+  //   email: new FormControl("", {nonNullable:true, validators: Validators.required}),
+  //   age: new FormControl("", {nonNullable:true, validators: [Validators.min(18), Validators.max(100)]}),
+  //   education: new FormControl("", {nonNullable:true, validators: Validators.required}),
+
+  // })
+
+  onSubmit() {
+    if(this.form.valid) {
+      console.log(this.form.value)
+      const person: EPerson = {
+        givenName: this.form.value.givenName ?? "",
+        surName: this.form.value.surName ?? "",
+        email: this.form.value.email ?? "",
+        age: String(this.form.value.age) ?? "",
+        education: this.form.value.education ?? ""
+      }
+      this.person.emit(person);
+      this.form.reset();
+    }
+
+    // console.log("Data: ", data)
+    // console.log("givenName>>>", this.form.controls["givenName"].value);
+    // this.form.controls["surName"].setValue("Papakis");
     
   }
 
