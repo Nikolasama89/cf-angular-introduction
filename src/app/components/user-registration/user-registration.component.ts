55342ff6e06f26e978cbc81c1c036abdb6edb021
Nikos <niqos89@gmail.com>
2025-05-07 21:08:53 +0300

    feat: form

--- src/app/components/user-registration/user-registration.component.ts
+++ src/app/components/user-registration/user-registration.component.ts
@@ -2,14 +2,29 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-user-registration',
-  imports: [MatInputModule, MatFormFieldModule, MatButtonModule],
+  imports: [MatInputModule, MatFormFieldModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './user-registration.component.html',
   styleUrl: './user-registration.component.css'
 })
 export class UserRegistrationComponent {
-
+  
+  form = new FormGroup({
+    username: new FormControl("", Validators.required),
+    name: new FormControl("", Validators.required),
+    surname: new FormControl("", Validators.required),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    // address is an object in the interface user
+    address: new FormGroup({
+      area: new FormControl(""),
+      road: new FormControl("")
+    }),
+    password: new FormControl("", [Validators.required, Validators.minLength(4)]),
+    confirmPassword: new FormControl("", [Validators.required, Validators.minLength(4)])
+  })
 }
