919b9482fa0c8aef0bcd72318fefccf94a07d9ca
Nikos <niqos89@gmail.com>
2025-05-13 20:40:29 +0300

    feat: guard for user registration componenent

---
+++ src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,16 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { UserService } from '../services/user.service';
+
+// guard for checking if user logged in , if not navigate user to login page
+export const authGuard: CanActivateFn = (route, state) => {
+  // const επειδη δεν ειμαστε μεσα σε class αλλα function
+  const userService = inject(UserService);
+  const router = inject(Router);
+  
+  if(userService.user$()) {
+    return true;
+  }
+
+  return router.navigate(["login"]);
+};
