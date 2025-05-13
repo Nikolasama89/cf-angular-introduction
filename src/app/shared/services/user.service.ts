6d4c30696ba12df0e6fc103e50f0f1870377b539
Nikos <niqos89@gmail.com>
2025-05-13 20:17:12 +0300

    feat: user login and user registration added

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,15 +1,22 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
-import { User } from '../interfaces/user';
+import { User, Credentials, LoggedInUser } from '../interfaces/user';
+import { Router } from '@angular/router';
+
 
 const API_URL = `${environment.apiURL}/api/users`
+const API_URL_AUTH = `${environment.apiURL}/api/auth`
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   http: HttpClient = inject(HttpClient)
+  router = inject(Router)
+
+  // convention for signal variables
+  user$ = signal<LoggedInUser | null>(null)
 
   registerUser(user: User) {
     return this.http.post<{status: boolean, data: User}>(`${API_URL}`, user)
@@ -17,5 +24,15 @@ export class UserService {
 
   check_duplicate_email(email: string) {
     return this.http.get<{status: boolean, data: User}>(`${API_URL}/check_duplicate_email/${email}`)
+  }
+
+  loginUser(credentials: Credentials) {
+    return this.http.post<{status:boolean, data: string}>(`${API_URL_AUTH}/login`, credentials)
+  }
+
+  logOutUser() {
+    this.user$.set(null);
+    localStorage.removeItem("access_token");
+    this.router.navigate(["login"]);
   }
 }
