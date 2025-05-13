16bf751b504d37dcb58bffcad8a711fd6d0be5dd
Nikos <niqos89@gmail.com>
2025-05-13 20:54:26 +0300

    feat: guard for admin role

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,6 +13,7 @@ import { UserLoginComponent } from './components/user-login/user-login.component
 
 // GUARDS
 import { authGuard } from './shared/guards/auth.guard';
+import { adminRoleGuard } from './shared/guards/admin-role.guard';
 
 export const routes: Routes = [
   { path: "for-directive-example", component: ForDirectiveExampleComponent },
@@ -23,7 +24,7 @@ export const routes: Routes = [
   { path: "template-driven-form-example", component: TemplateDrivenFormExampleComponent},
   { path: "reactive-form-example", component: ReactiveFormExampleComponent},
   { path: "http-client-example", component: HttpClientExampleComponent},
-  { path: "user-registration-example", component: UserRegistrationComponent, canActivate: [authGuard]}, // if not pass redirect
+  { path: "user-registration-example", component: UserRegistrationComponent, canActivate: [authGuard, adminRoleGuard]}, // if not pass redirect
   { path: "login", component: UserLoginComponent},
   { path:"welcome", component: WelcomeComponent },
   { path: "", redirectTo:"/welcome", pathMatch:"full" }
